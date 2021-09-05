0d15c624f8154a615aafab922349cc180c3a66e4
Danny He <dannyhe@hotmail.co.uk>
2021-09-05 16:15:09 +0100

    changed response for user related requests for front end use

--- controllers/users.js
+++ controllers/users.js
@@ -32,19 +32,19 @@ export const logIn = async (req, res) => {
     const user = await User.findOne({ username });
     if (user === null) {
       // Wrong Username
-      res.status(403).json(false);
+      res.status(403).json({ success: false });
     } else {
       const key = new NodeRSA(process.env.PRIVATE_KEY);
       const decryptedPassword = key.decrypt(encryptedPassword, "utf8");
       if (stringHash(decryptedPassword) === user.password) {
-        res.status(200).json(true);
+        res.status(200).json({ success: true, username, id: user._id });
         // Wrong Password
       } else {
-        res.status(401).json(false);
+        res.status(401).json({ success: false });
       }
     }
   } catch (error) {
-    res.status(404).json(false);
+    res.status(404).json(error);
   }
 };
 
@@ -54,7 +54,7 @@ export const createUser = async (req, res) => {
     const user = await User.findOne({ username });
     if (user !== null) {
       //Username Taken
-      res.status(403).json(false);
+      res.status(403).json({ success: false });
     } else {
       const key = new NodeRSA(process.env.PRIVATE_KEY);
       const decryptedPassword = key.decrypt(encryptedPassword, "utf8");
@@ -63,7 +63,7 @@ export const createUser = async (req, res) => {
         password: stringHash(decryptedPassword),
       });
       await newUser.save();
-      res.status(201).json(true);
+      res.status(201).json({ success: true, username, id: newUser._id });
     }
   } catch (error) {
     res.status(409).json(error);
